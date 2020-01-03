ef78f7dfc39eedc8e7540f12deeb39b9b6953ea9
吕泰鹏 <@DESKTOP-P083CPV>
2020-01-03 08:15:44 +0800

    修改项目代码，再次提交Git仓库

--- src/main/java/com/lvtaipeng/test/Test.java
+++ src/main/java/com/lvtaipeng/test/Test.java
@@ -3,6 +3,6 @@ package com.lvtaipeng.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("helloWord¡£");
+		System.out.println("helloWord"+"ĞŞ¸Ä´úÂë");
 	}
 }
